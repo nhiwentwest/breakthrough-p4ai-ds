28fb1080d8262e910fb78207abc7ebe3c4000d8b
“nhiwentwest” <phantranynhi@hotmail.com>
2026-04-19 02:42:02 +0700

    update image demo and training scripts

--- streamlit_app/pages/demo2_image.py
+++ streamlit_app/pages/demo2_image.py
@@ -363,7 +363,6 @@ def load_model_and_labels(model_choice: str, ckpt_path: str, map_path: str):
             dropout=cfg.get("dropout", 0.3),
             pretrained=True,
             freeze_backbone=True,
-            head_style="sequential",
         ).to(device)
     elif model_choice == "Pretrained CNN Fine-tuned":
         model = build_resnet50_classifier(
@@ -371,7 +370,6 @@ def load_model_and_labels(model_choice: str, ckpt_path: str, map_path: str):
             dropout=cfg.get("dropout", 0.3),
             pretrained=True,
             freeze_backbone=False,
-            head_style="linear",
         ).to(device)
     else:
         model = CNNScratch(
